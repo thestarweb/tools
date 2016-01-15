519d499d0d60bc99062fe5c5b13995d9d5b737a8
thestarweb <597577939@qq.com>
2016-01-15 18:50:15 +0800

    分页工具优化

--- fenye_tool.php
+++ fenye_tool.php
@@ -2,15 +2,18 @@
 class fenye_tool{
 	public $out='';
 	public $now_page=' {page} ';
+	public $go='';
 	/**
-		@ max 最大页
+		@ max 最大页(最大记录条数)
 		@ now 当前页
 		@ url 连接地址
+		@ limit 单页最大记录数（如果为false，max表示最大页）
 	*/
-	public function __construct($max,$now,$url=''){
-		$this->max=$max;
+	public function __construct($max,$now,$url='',$limit=false){
+		$this->max=$limit?ceil($max/$limit):$max;
 		$this->now=$now;
 		$this->out=' <a href="'.$url.'{page}">[{page}]</a> ';
+		$this->go=' <a href="'.$url.'{page}">{name}</a> ';
 	}
 	/**
 		return 分页数据（网页下方的那些按钮）
@@ -18,7 +21,7 @@ class fenye_tool{
 	public function get(){
 		//var_dump($this->max==0,$this->max<$this->now,$this->now<1);exit;
 		if($this->max==0||$this->max<$this->now||$this->now<1) return '';
-		$re='';
+		$re=$this->now==1?'':str_replace('{page}',$this->now-1,str_replace('{name}','上一页',$this->go));
 		$startpage=$this->now<5?1:$this->now-4;
 		$endpage=$this->now<$this->max-5?$this->now+5:$this->max;
 		for($i=$startpage;$i<=$endpage;$i++){
@@ -28,6 +31,6 @@ class fenye_tool{
 				$re.=str_replace('{page}',$i,$this->out);
 			}
 		}
-		return $re."($this->now/$this->max)";
+		return $re."($this->now/$this->max)".($this->now==$this->max?'':str_replace('{page}',$this->now+1,str_replace('{name}','下一页',$this->go)));
 	}
 }
