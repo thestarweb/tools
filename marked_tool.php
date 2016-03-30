7dac9baaee6fd119276c518ae002d53fc0a5873e
thestarweb <597577939@qq.com>
2016-03-30 22:54:23 +0800

    marked_tool更新

--- marked_tool.php
+++ marked_tool.php
@@ -1,13 +1,14 @@
 <?php
 class marked_tool{
 	public static function marked($mk){
-		$mk=preg_replace_callback ('/(?:\r|\n)(#+)(.*?)(\r|\n)/',function($c){
+		$mk=preg_replace_callback ('/(?:\r|\n|^)(#+)(.*?)(\r|\n)/',function($c){
 			$h=strlen($c[1]);//var_dump($c);
 			return "\n\n<h".$h.'>'.$c[2].'</h'.$h.">\n\n";
 		}, $mk);
+		$mk=preg_replace('/\$\[(.*?)\]\((.*?)((?: w=\d%?)?)((?: h=\d%?)?)\)/','<span style="color:$2">$1</span>', $mk);
 		$mk=preg_replace('/!\[(.*?)\]\((.*?)((?: w=\d%?)?)((?: h=\d%?)?)\)/','<img src="$2" title="$1" style="width=$3;height:$4"/>', $mk);
 		$mk=preg_replace('/\[(.*?)\]\((.*?)\)/','<a href="$2">$1</a>', $mk);
-		$mk=preg_replace_callback('/((?:<.+?>)?)(\n)((?:<.+?>)?)/',function($c){
+		$mk=preg_replace_callback('/((?:<[^<]+?>)?)(\n+)((?:<.+?>)?)/',function($c){
 			$res=$c[1];
 			if(strpos($c[1],'</h')!==0)$res.='</p>';
 			if(strpos($c[3],'<h')!==0) $res.='<p>';
