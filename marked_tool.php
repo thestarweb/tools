bf2c8d2e5961f48de26b42f7d5c0828c8d8a471f
thestarweb <597577939@qq.com>
2016-04-18 22:10:37 +0800

    marked解析优化

--- marked_tool.php
+++ marked_tool.php
@@ -1,12 +1,15 @@
 <?php
 class marked_tool{
-	public static function marked($mk){
+	public static function marked($mk,$ob=array()){
+		foreach($ob as $k=>$v){
+			$mk=str_replace('{$'.$k.'}',$v,$mk);
+		}
 		$mk=preg_replace_callback ('/(?:\r|\n|^)(#+)(.*?)(\r|\n)/',function($c){
 			$h=strlen($c[1]);//var_dump($c);
 			return "\n\n<h".$h.'>'.$c[2].'</h'.$h.">\n\n";
 		}, $mk);
-		$mk=preg_replace('/\$\[(.*?)\]\((.*?)((?: w=\d%?)?)((?: h=\d%?)?)\)/','<span style="color:$2">$1</span>', $mk);
-		$mk=preg_replace('/!\[(.*?)\]\((.*?)((?: w=\d%?)?)((?: h=\d%?)?)\)/','<img src="$2" title="$1" style="width=$3;height:$4"/>', $mk);
+		$mk=preg_replace('/\$\[(.*?)\]\((.*?)\)/','<span style="color:$2">$1</span>', $mk);
+		$mk=preg_replace('/!\[(.*?)\]\((.*?)(?: w=(\d+%?))?(?: h=(\d+%?))?\)/','<img src="$2" title="$1" style="width:$3;height:$4"/>', $mk);
 		$mk=preg_replace('/\[(.*?)\]\((.*?)\)/','<a href="$2">$1</a>', $mk);
 		$mk=preg_replace_callback('/((?:<[^<]+?>)?)(\n+)((?:<.+?>)?)/',function($c){
 			$res=$c[1];
