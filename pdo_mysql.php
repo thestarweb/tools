cd513318bb115fc8bef96f17e35f5e7a58dd2795
thestarweb <597577939@qq.com>
2016-10-18 19:43:02 +0800

    pdo_mysql命名问题修复准备

--- pdo_mysql.php
+++ pdo_mysql.php
@@ -23,6 +23,39 @@
 		/**
 			@sql string 要执行的sql语句
 		*/
+		public function execute($sql){
+			$sql=str_replace('@%_',$this->prefix,$sql);
+			list($doing)=explode(' ',$sql);
+			$doing=strtoupper($doing);
+			if($doing=='SELECT'||$doing=='SHOW'||$doing=='DESC'){
+				$res=$this->pdo->query($sql);
+				$error=$this->pdo->errorInfo();
+				if($error[1]){
+					trigger_error('mysql_tool error:'.$error[1].'故障信息'.$error[2],512);
+				}
+				if(is_object($res)){
+					$res->setFetchMode(PDO::FETCH_ASSOC);
+					return $res->fetchAll();
+				}else{
+					if($res===false){
+						var_dump($this->pdo->errorInfo());
+					}
+					return $res;
+				}
+			}else{
+
+				$res=$this->pdo->exec($sql);
+				$error=$this->pdo->errorInfo();
+				if($error[1]){
+					trigger_error('mysql_tool error:'.$error[1].'故障信息'.$error[2],512);
+				}
+				return $res;
+			}
+		}
+		//下面函数与上相同，但由于命名原因，已经不推荐使用，未来将会改为调用上面的方法、
+		/**
+			@sql string 要执行的sql语句
+		*/
 		public function do_SQL($sql){
 			$sql=str_replace('@%_',$this->prefix,$sql);
 			list($doing)=explode(' ',$sql);
@@ -52,9 +85,36 @@
 				return $res;
 			}
 		}
+
+		//预编译一个sql语句
+		/**
+			@sql string 需要预编译的sql语句
+		*/
 		public function prepare($sql){
 			return $this->pdo->prepare($sql);
 		}
+
+		//支持有用户输入参数过滤的sql语句执行方法
+		/**
+			有用户输入时的sql执行方案（使用了预编译机制防止sql注入）
+			@sql string 进行预编译的语句
+			@arr array 用于替换sql中“？”的数组
+			@fetch_type int 返回数组的格式
+			return mix
+				查询语句 array 检索出的数据
+				插入等语句 int 是否执行成功
+		*/
+		public function u_execute($sql,$arr,$fetch_type=PDO::FETCH_ASSOC){
+			$sth=$this->prepare(str_replace('@%_',$this->prefix,$sql));
+			$sth->execute($arr);
+			$error=$sth->errorInfo();
+			if($error[1]){
+				trigger_error('mysql_tool error:'.$error[1].'语句'.$sql.'故障信息'.$error[2],512);
+			}
+			$sth->setFetchMode(PDO::FETCH_ASSOC);
+			return($sth->fetchAll($fetch_type));
+		}
+		//下面函数与上相同，但由于命名原因，已经不推荐使用，未来将会改为调用上面的方法、
 		/**
 			有用户输入时的sql执行方案（使用了预编译机制防止sql注入）
 			@sql string 进行预编译的语句
