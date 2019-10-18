a932f674c149ad15a9d643c9edbc2e5c9c914983
thestarweb <597577939@qq.com>
2019-10-18 16:58:29 +0800

    增加完整markdown解析功能（暂时只完成了块级解析）

--- marked_tool.php
+++ marked_tool.php
@@ -26,4 +26,108 @@ class marked_tool{
 		$h=strlen($j);
 		return '<h'.$h.'>'.$title.'</h'.$h.'>';
 	}
+	public static $block_rule;
+	public static function marked_all($mk,$ob=[],$m_flag=0){
+		//Lexer.prototype.lex
+		$mk=str_replace("\r","\n",str_replace("\r\n","\n",$mk));
+		//Lexer.prototype.token
+		$res='';
+		while($mk){
+			$flag=false;
+			foreach (marked_tool::$block_rule as $k => $v) {
+				if(preg_match($v[0],$mk,$cap)){
+					$flag=true;
+					if(isset($v[1]))$res.=($v[1])($cap,$ob,$m_flag);//Parser.prototype.tok
+					$mk=substr($mk,strlen($cap[0]));
+					break;
+				}
+			}
+			if(!$flag) throw new Exception('marked_tool解析出错',512);
+		}
+		return $res;
+	}
+	public static function marked_inline($t){
+		return $t;
+	}
+
+	public static function init(){
+		self::$block_rule=[
+			"newline"=>["/^\n/",function($cap){
+				return '';
+			}],//空行
+			"code"=>["/^( {4}[^\n]+\n*)+/",function($cap){
+				return '<pre style="overflow:auto;"><code><ol><li>'.str_replace("\n",'</li><li>',$cap[0]).'</li></ol></code></pre>';
+			}],
+			"fences"=>["/^ *(`{3,}|~{3,}) *(\w*) *\n([\s\S]+?)\s*\\1 *(?:\n+|$)/",function($cap){
+				return '<pre style="overflow:auto;"><code class="'.$cap[2].'"><ol><li>'.str_replace("\n",'</li><li>',$cap[3]).'</li></ol></code></pre>';
+			}],//代码段
+			"hr"=>["/^( *[-*_]){3,} *(?:\n+|$)/",function($cap){
+				return '<hr/>';
+			}],//分隔符
+			"heading"=>["/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/",function($cap,$cfg=[],$flag=0){
+				$depth=strlen($cap[1]);
+				return '<h'.$depth.'>'.marked_tool::marked_inline($cap[2],$cfg).'</h'.$depth.'>';
+			}],//标题
+			//"nptable"=>["/^ *(\S.*\|.*)\n *([-:]+ *\|[-| :]*)\n((?:.*\|.*(?:\n|$))*)\n*/"],
+			//"lheading"=>["/^([^\n]+)\n *(=|-){3,} *\n*/"],
+			"blockquote"=>["/^( *>[^\n]+(\n[^\n]+)*\n*)+/",function($cap,$cfg=[],$flag=0){
+				return '<blockquote>'.marked_tool::marked_all(preg_replace('/^ *> ?/',$cap[0]),$cfg,$flag).'</blockquote>';
+			}],
+			"list"=>["/^( *)([*+-]|\d+\.) [\s\S]+?(?:\n+(?=(?: *[-*_]){3,} *(?:\n+|$))|\n{2,}(?! )(?!\\1(?:[*+-]|\d+\.) )\n*|\s*$)/",function($cap,$cfg=[],$flag=0){
+				if(in_array($cap[2], ['*','+','-'])){
+					$type='ul';
+				}else{
+					$type='ol';
+				}
+				$res='';
+				preg_match_all("/^( *)([*+-]|\d+\.) [^\n]*(?:\n(?!\\1(?:[*+-]|\d+\.) )[^\n]*)*/m",$cap[0],$cap);
+				foreach ($cap[0] as $key => $value) {
+					$value=preg_replace('/^ *([*+-]|\d+\.) +/','',$value);
+					$res.='<li>'.marked_tool::marked_all($value,$cfg,$flag|0x10000).'</li>';
+				}
+				return '<'.$type.'>'.$res.'</'.$type.'>';
+			}],//列表
+			//"html"=>"/^ *(?:comment|closed|closing) *(?:\n{2,}|\s*$)/",
+			"def"=>["/^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +[\"(]([^\n]+)[\")])? *(?:\n+|$)/",function($cap,$cfg=[],$flag=0){
+				//
+			}],
+			"table"=>["/^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/",function($cap){
+				$body='<thead><tr>';
+				$header=preg_split('/ *\| */',preg_replace('/^ *| *\| *$/','',$cap[1]));
+				$align=preg_split('/ *\| */',preg_replace('/^ *|\| *$/','',$cap[2]));
+				$cells=explode("\n",preg_replace("/(?: *\| *)?\n$/",'',$cap[3]));
+				foreach ($align as $k=>$v) {
+					if(preg_match('/^ *-+: *$/',$v)){
+						$align[$k]='right';
+					}else if(preg_match('/^ *:-+: *$/',$v)){
+						$align[$k]='center';
+					}else if(preg_match('/^ *:-+ *$/',$v)){
+						$align[$k]='left';
+					}else{
+						$align[$k]='';
+					}
+				}
+				foreach ($header as $k=>$v) {
+					$body.=($align[$k]?('<th align="'.$align[$k].'">'):'<th>').marked_tool::marked_inline($v).'</th>';
+				}
+				$body.='</tr></thead><tbody>';
+				foreach($cells as $line) {
+					$c=preg_split('/ *\| */',preg_replace('/^ *\| *| *\| *$/','',$line));
+					$body.='<tr>';
+					foreach ($c as $k=>$v) {
+						$body.=($align[$k]?('<td align="'.$align[$k].'">'):'<td>').marked_tool::marked_inline($v).'</td>';
+					}
+					$body.='</tr>';
+				}
+				$body.='</tbody>';
+				return '<table>'.$body.'</table>';
+			}],//表格
+			//"paragraph"=>"/^((?:[^\n]+\n?(?!hr|heading|lheading|blockquote|tag|def))+)\n*/",
+			"text"=>["/^[^\n]+/",function($cap,$cfg=[],$flag=0){
+				if($flag&0x10000) return marked_tool::marked_inline($cap[0],$cfg);
+				return'<p>'.marked_tool::marked_inline($cap[0],$cfg).'</p>';
+			}]//普通文本
+		];
+	}
 }
+marked_tool::init();
